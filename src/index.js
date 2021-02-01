0e6a45fadbb1371555ce3142d3dbd30390acb377
Ranto p <rantoprianto21@gmail.com>
2021-02-01 21:34:07 +0700

    menambahkan packages axios,redux dan semantic ui

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Routes from './Routes';
+import 'semantic-ui-css/semantic.min.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <Routes />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
